7d7f3adf0df0fe5d139237b626ee0f73ac368d7e
Akobir Rustamov <rustamovakobir8434@gmail.com>
2026-03-24 15:54:23 +0500

    filter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  console.log("salom1");
-  
+ 
   const [data, setData] = useState(
     [
     {id:1, name: "Asil", age:20},
@@ -18,6 +17,18 @@ function App() {
   ]
 )
 
+
+  function handleDelete(id){
+      
+
+
+      setData(
+        data.filter((item)=>{
+          return item.id != id
+        }))
+      
+    
+  }
   return (
     <div className="App">
      <h1 className='text-3xl my-4 font-bold '>1-24 ATT guruh talabalari reactjs darsida </h1>
@@ -26,9 +37,10 @@ function App() {
 
       {
         data.map((item)=>
-          <div key={item.id} className='flex justify-between border-b-2'>
+          <div key={item.id} className='flex justify-between border-b-2 px-4 py-2'>
              <p>{item.name}</p>
              <p>{item.age}</p>
+             <p onClick={()=>handleDelete(item.id)}><i className="fa-solid fa-trash"></i></p>
           </div>
         )
           
