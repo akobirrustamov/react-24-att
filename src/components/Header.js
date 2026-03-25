67112519fe7c87a8f92891059de0e27b2ab33a96
Akobir Rustamov <rustamovakobir8434@gmail.com>
2026-03-25 15:59:09 +0500

    components

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+export default function Header() {
+  return (
+    
+      <div className='flex w-7xl justify-between m-auto bg-blue-400 py-4 px-2 text-white'>
+        <div>logo</div>
+
+        <div className='flex gap-6'>
+          <p>Home</p>
+           <p>About</p>
+            <p>Countact us</p>
+             <p>Home</p>
+              <p>Home</p>
+        </div>
+
+        <div>
+          iconlar
+        </div>
+      </div>
+  )
+}
