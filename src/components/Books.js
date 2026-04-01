b100e4602b9727b0d42004c68ad21ec78ef1ab9d
Akobir Rustamov <rustamovakobir8434@gmail.com>
2026-04-01 18:02:36 +0500

    ready

--- src/components/Books.js
+++ src/components/Books.js
@@ -53,7 +53,7 @@ export default function Books() {
             Badiiy kitoblar. O'qing va orom oling
         </div>
 
-        <div className='px-8 py-4 grid grid-cols-4 gap-4'>
+        <div className='px-8 py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
 
 
                 {
